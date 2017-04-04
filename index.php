d8dff249e7f37eb18d3666539015f8b80f8eacd8
neelsaraiya <neelsaraiya@nbnco.com.au>
2017-04-05 01:13:18 +1000

    closed script tags

--- index.php
+++ index.php
@@ -8,8 +8,8 @@ $sql = "select * from timings  where date >=  CURDATE() order by date asc";
 $result = $conn->query($sql);
 ?>
 <html>
-<script src="https://code.jquery.com/jquery-1.12.4.js" />
-<script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js" />
+<script src="https://code.jquery.com/jquery-1.12.4.js" ></script>
+<script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js" ></script>
 <link href="https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css" />
 <body>
 <table id="example" class="display" cellspacing="0" width="100%">

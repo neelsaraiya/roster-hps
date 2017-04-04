ff72a14f28d5b8a4773de89780137cfa470d591f
neelsaraiya <neelsaraiya@nbnco.com.au>
2017-04-05 01:12:14 +1000

    added html tags

--- index.php
+++ index.php
@@ -3,14 +3,15 @@
 include 'functions.php';
 
 $conn = dbconn();
-$sql = "DELETE FROM timings;";
 
 $sql = "select * from timings  where date >=  CURDATE() order by date asc";
 $result = $conn->query($sql);
 ?>
+<html>
 <script src="https://code.jquery.com/jquery-1.12.4.js" />
 <script src="https://cdn.datatables.net/1.10.13/js/jquery.dataTables.min.js" />
 <link href="https://cdn.datatables.net/1.10.13/css/jquery.dataTables.min.css" />
+<body>
 <table id="example" class="display" cellspacing="0" width="100%">
     <thead>
     <tr>
@@ -35,6 +36,8 @@ $conn->close();
 ?>
     </tbody>
 </table>
+</body>
+</html>
 <script>
     $(document).ready(function() {
         $('#example').DataTable();
